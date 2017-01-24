7f232c733d307d66ede214bf4b0f9e03db131b5d
akshay <akshayy.yadav@gmail.com>
2017-01-23 19:00:40 -0600

    week2

--- week2/ushcn.R
+++ week2/ushcn.R
@@ -3,4 +3,4 @@ temps <- read_fwf("ushcn-v2.5-stations.txt",col_positions = fwf_positions(
                     start=c(1,3,4,6,13,22,33,39,42,73,80,87,94), 
                     end  =c(2,3,5,11,20,30,37,40,71,78,85,92,95)))
 names(temps) <- c("COUNTRY", "NETWORK","ID","COOP_ID","LATITUDE","LONGITUDE","ELEVATION","STATE","NAME","COMP1","COMP2","COMP3","UTC_OFF") 
-temps%>% ggplot(aes(x = LONGITUDE, LATITUDE)) + geom_point() 
+temps%>% ggplot(aes(x = LONGITUDE, LATITUDE)) + geom_point()
